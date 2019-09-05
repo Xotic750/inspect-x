f53bf94714bb675e27c4a081852570ac284bfec1
Graham Fairweather <xotic750@gmail.com>
2019-09-05 22:32:46 +0200

    Maintenance

--- dist/inspect-x.js
+++ dist/inspect-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-2017",
-  "date": "2019-09-05T00:20:10.703Z",
+  "date": "2019-09-05T20:32:36.499Z",
   "describe": "",
   "description": "An implementation of node's ES6 inspect module.",
   "file": "inspect-x.js",
